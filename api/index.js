12456a78fb4a43ee248cf92a22d53f1161a39571
amro22397 <amroalmutasim22@gmail.com>
2024-10-01 21:02:38 +0400

    fix backend and deployment config 2

--- api/index.js
+++ api/index.js
@@ -35,10 +35,10 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listings", listingRouter)
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '/Client/dist')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'Client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
